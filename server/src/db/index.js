4c9046030aa21e8b17e9e37174106aecca8b4d85
dooly <sonaky47@gmail.com>
2017-11-03 20:28:39 +0900

    Backend 노드서버, mongodb connect !

---
+++ server/src/db/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('mongoose-double')(mongoose);
+const log = require('lib/log');
+
+const {
+    MONGO_URI: mongoURI
+} = process.env;
+
+module.exports = (function () {
+    mongoose.Promise = global.Promise;
+
+    return {
+        connect() {
+            return mongoose.connect(mongoURI, {
+                useMongoClient: true
+            }).then(
+                () => {
+                    log.info('Successfully connected to mongodb');
+                }
+            ).catch(e => {
+                log.error(e);
+            })
+        }
+    }
+})();
---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const {
+    PORT: port
+} = process.env;
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const db = require('./db');
+db.connect();
+
+const api = require('./api');
+
+const app = new Koa();
+app.use(bodyParser());
+const router = new Router();
+router.use('/api', api.routes());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+app.listen(port, () => {
+    console.log(`everydaymemo server is listening to port ${port}`);
+})
