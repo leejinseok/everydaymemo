c127cdf69d38829129cdccc5e735079231bc7e11
dooly <sonaky47@gmail.com>
2017-11-08 12:55:35 +0900

    Proxy가  정상화 되었음 ㅜㅜ

--- server/src/api/v1.0/auth/auth.ctrl.js
+++ server/src/api/v1.0/auth/auth.ctrl.js
@@ -3,5 +3,14 @@ const User = require('db/models/User');
 exports.checkEmail = async (ctx) => {
     const { email } = ctx.params;
 
-    ctx.body = email;
+    try {
+        const account = await User.findByEmail(email);
+        ctx.body = {
+          exists: !!account
+        };
+    } catch (e) {
+        
+    }
+
+    
 }
--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ const {
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 
 const db = require('./db');
 db.connect();
@@ -18,6 +19,7 @@ const router = new Router();
 router.use('/api', api.routes());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(cors());
 
 
 app.listen(port, () => {
